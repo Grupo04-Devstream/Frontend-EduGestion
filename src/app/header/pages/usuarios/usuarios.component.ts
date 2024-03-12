ba10acc76977679fd1bd860fb02016d2e976bca8
Jmond544 <jmondalgotapia@gmail.com>
2024-03-12 11:53:54 -0500

    Add users

---
+++ src/app/header/pages/usuarios/usuarios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './usuarios.component.html',
+  styleUrl: './usuarios.component.css'
+})
+export class UsuariosComponent {
+
+}
