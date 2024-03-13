37c0e28d13a71d7f78b06ea7bbba7f20d63daaac
Jmond544 <jmondalgotapia@gmail.com>
2024-03-12 23:17:04 -0500

    Update home component HTML and TypeScript files

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,13 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './home.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: "app-home",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "./home.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class HomeComponent { }
+export default class HomeComponent {
+    public users = [
+        "Aco Tito, Anthony Edwin",
+        "Hilario Soto, Jheisson Rudy",
+        "Lavado Torres, Gianmarco Gabriel",
+        "Mondalgo Tapia, Juan Carlos",
+    ];
+}
