8a156b8df292a10457c47fd282ee1ae94f9b57e3
Jmond544 <jmondalgotapia@gmail.com>
2024-03-13 01:53:23 -0500

    Add delete and edit functionality to user table

--- src/app/dashboard/pages/usuarios/table/table.component.ts
+++ src/app/dashboard/pages/usuarios/table/table.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { UsuariosResponse } from '../../../../interfaces/req-response';
+import { Component, Input } from "@angular/core";
+import { UsuariosResponse } from "../../../../interfaces/req-response";
 
 @Component({
-  selector: 'app-table',
-  standalone: true,
-  imports: [],
-  templateUrl: './table.component.html',
+    selector: "app-table",
+    standalone: true,
+    imports: [],
+    templateUrl: "./table.component.html",
 })
 export class TableComponent {
-  @Input() data: UsuariosResponse[] = [];
+    @Input() data: UsuariosResponse[] = [];
+
+    deleteUser(id: number) {
+        alert("Usuario eliminado: Coming soon...");
+    }
+
+    editUser(id: number) {
+        alert("Usuario editado: Coming soon...");
+    }
 }
