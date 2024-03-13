3a3186085e078e82cc3a5abb6e8b1c7d5dc60d6d
JRhoud <jhs.houd@gmail.com>
2024-03-13 11:20:44 -0500

    asd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-  importProvidersFrom(
+    importProvidersFrom(
     HttpClientModule,
   )]
 };
